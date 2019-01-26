bca924f087f13e916e0b7c15ea074b924cfc736a
Alex Siman <aleksandr.siman@gmail.com>
2019-01-26 15:20:47 +0200

    Describe and register Joystream custom types, structs and enums

---
+++ src/joystream-types.js
@@ -0,0 +1,52 @@
+// See https://github.com/paritytech/oo7/blob/master/packages/oo7-substrate/src/codec.js
+export const JoystreamTypes = {
+	ProposalId: 'u32',
+	ProposalStatus: { _enum: [ 
+		'Pending', 
+		'Cancelled', 
+		'Expired', 
+		'Approved', 
+		'Rejected', 
+		'Slashed',
+	]},
+	'Proposal<AccountId,Balance,BlockNumber>': {
+		id: 'ProposalId',
+		proposer: 'AccountId',
+		stake: 'Balance',
+		name: 'Vec<u8>',
+		description: 'Vec<u8>',
+		wasm_code: 'Vec<u8>',
+		proposed_on: 'BlockNumber',
+		status: 'ProposalStatus',
+	},
+	VoteKind: { _enum: [ 
+		'Abstention', 
+		'Approve', 
+		'Reject', 
+		'Slash',
+	]},
+	'TallyResult<BlockNumber>': {
+		proposal_id: 'ProposalId',
+		abstentions: 'u32',
+		approvals: 'u32',
+		rejections: 'u32',
+		slashes: 'u32',
+		status: 'ProposalStatus',
+		finalized_on: 'BlockNumber',
+	},
+};
+
+/** Type will not be recognized by Parity codec if there are spaces in its name. */
+function removeSpaces(str) {
+    return typeof str === 'string' ? str.replace(/[\s]+/g, '') : str;
+}
+
+/** Register custom types of Joystream Node.*/
+export function registerJoystreamTypes() {
+    Object.keys(JoystreamTypes).forEach((typeName) => {
+        addCodecTransform(
+            removeSpaces(typeName), 
+            removeSpaces(JoystreamTypes[typeName])
+        );
+    });
+};
